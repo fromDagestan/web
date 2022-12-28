af0d613dcd44e8be3584be226a49643fd7cc7504
fromDagestan <121576854+fromDagestan@users.noreply.github.com>
2022-12-28 22:02:24 +0300

    Add files via upload

---
+++ registr/assets/index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>reg</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <!-- Форма авторизации -->
+
+    <form action="" method="">
+        <label>логин</label>
+        <input type="text" placeholder="введите свой логин">
+        <label>пароль</label>
+        <input type="password" placeholder="введите пароль">
+        <button>Войти</button>
+        <p>
+            у вас нет аккаунта? - <a href="reg.php">зарегистрируйтесь</a>!
+        </p>
+    </form>
+</body>
+</html>
---
+++ registr/assets/reg.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>reg</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <!-- Форма регистрации -->
+
+    <form action="vendor/signup.php" method="post" enctype="multipart/form-data">
+        <label>ФИО</label>
+        <input type="text" name="full_name" placeholder="введите своё полное имя">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="введите свой логин">
+        <label>Почта</label>
+        <input type="email" name="email" placeholder="введите адрес своей почты">
+        <label>Пароль</label>
+        <input type="text" name="password" placeholder="введите пароль">
+        <label>Подтверждение пароля</label>
+        <input type="text" name="password_confirm" placeholder="подтвердите пароль">
+        <label>Изображение профиля</labe>
+        <input type="file" name="avatar">
+        <button ><a href="vendor/signup.php">войдите</a>зарегистрироваться</button>
+        <p>
+           у вас усть аккаунт? - <a href="index.php">войдите</a>!
+        </p>
+    </form>
+</body>
+</html>
